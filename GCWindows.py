c05f7846158817381b64cd0046b63da49973b141
Lemonsizzle <70922389+Lemonsizzle@users.noreply.github.com>
2023-09-03 12:25:54 +1200

    Implemented basic windows functionality

---
+++ GCWindows.py
@@ -0,0 +1,45 @@
+import win32api
+import win32con
+import win32gui
+import wmi
+import ctypes
+
+
+class WindowsControl:
+    def __init__(self):
+        pass
+
+    def scroll(self, value):
+        # Scroll up or down; positive value scrolls up, negative scrolls down
+        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, value)
+        #print(f"Scrolling by {value}")
+
+    def set_volume(self, level):
+        # Set the volume level (0.0 to 1.0)
+        devices = ctypes.windll.winmm.waveOutGetVolume(0)
+        volume = int(level * 0xFFFF) & 0xFFFF
+        ctypes.windll.winmm.waveOutSetVolume(0, volume | (volume << 16))
+
+    def set_brightness(self, level):
+        # Set the brightness level (0 to 100)
+        c = wmi.WMI(namespace='wmi')
+        methods = c.WmiMonitorBrightnessMethods()[0]
+        methods.WmiSetBrightness(level, 0)
+
+    def zoom(self, direction):
+        # Zoom in or out; positive direction zooms in, negative zooms out
+        if direction > 0:
+            win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
+            win32api.keybd_event(win32con.VK_ADD, 0, 0, 0)
+            win32api.keybd_event(win32con.VK_ADD, 0, win32con.KEYEVENTF_KEYUP, 0)
+            win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
+        else:
+            win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
+            win32api.keybd_event(win32con.VK_SUBTRACT, 0, 0, 0)
+            win32api.keybd_event(win32con.VK_SUBTRACT, 0, win32con.KEYEVENTF_KEYUP, 0)
+            win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
+
+
+if __name__ == "__main__":
+    winControl = WindowsControl()
+    winControl.scroll(300)
--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import math
 import os
 import re
 import signal
+import threading
 import time
+import json
 
 import cv2
 import mediapipe as mp
@@ -11,9 +13,11 @@ from HandPositionIdentifier import HPI
 from RPS import RPS
 import HandModelMaker as HMM
 import GCDatabase
-from flask import Flask, render_template, Response, request, jsonify
+from GCWindows import WindowsControl
+from flask import Flask, render_template, Response, request, jsonify, abort
 from flask_socketio import SocketIO
 from google.protobuf.json_format import MessageToDict
+from collections import deque
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -23,6 +27,8 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 
 db = GCDatabase.database("data.db")
+
+control_win = WindowsControl()
 
 cap = cv2.VideoCapture(0)
 
@@ -42,10 +48,33 @@ v_flip = False
 
 mode = "off"
 
+config_name = "config.json"
+config_data = {}
+
+if os.path.exists(config_name):
+    with open(config_name, 'r+') as config_file:
+        config_content = config_file.read()
+        if config_content and 'src' in config_content:
+            config_data = json.loads(config_content)
+            cap = cv2.VideoCapture(config_data['src'])
+
+lock = threading.Lock()
+
+"""# "time":0, maybe
+    "wrist": [],
+    "thumb": [],
+    "index": [],
+    "middle": [],
+    "ring": [],
+    "pinky": [],
+    "finger_pos": []"""
+#history = []
+history = deque(maxlen=10)
+
 
 @app.route('/change_source', methods=['POST'])
 def change_source(src=None):
-    global cap
+    global cap, lock
     if src is None:
         src = request.form.get('src')
 
@@ -57,13 +86,19 @@ def change_source(src=None):
 
     ip_port = r"^\d+\.\d+\.\d+\.\d+:\d+$"
 
-    if re.match(ip_port, src):
-        parts = src.split(":")
-        src = f"http://{parts[0]}:{parts[1]}"
+    if type(src) is not int:
+        if re.match(ip_port, src):
+            parts = src.split(":")
+            src = f"http://{parts[0]}:{parts[1]}"
 
-    if cap.isOpened():
-        cap.release()
-    cap = cv2.VideoCapture(src)
+    # Critical section of code that only one thread can access at a time
+    print("Thread entering critical section (Changing source)")
+    with lock:
+        if cap.isOpened():
+            cap.release()
+        cap = cv2.VideoCapture(src)
+
+    config_data['src'] = src
 
     return jsonify(success=True, message=f"Camera source changed to {src}")
 
@@ -114,9 +149,18 @@ def distance(point, origin):
     float
         The Euclidean distance between the two points.
     """
-    x1, y1, z1 = point
-    x2, y2, z2 = origin
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+    if len(point) == 3:
+        x1, y1, z1 = point
+        x2, y2, z2 = origin
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+    if len(point) == 2:
+        x1, y1 = point
+        x2, y2 = origin
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    if len(point) == 1:
+        x1 = point[0]
+        x2 = origin[0]
+        return math.sqrt((x2 - x1) ** 2)
 
 
 @app.route('/record', methods=['POST'])
@@ -124,9 +168,7 @@ def record():
     global resultHand  # , db
 
     # TODO: implement buttons for recording, also js for generating buttons based on classes
-    # shape = request.form.get('shape')
-    shape = None
-    return
+    shape = request.form.get('shape')
 
     if resultHand:
         if resultHand.multi_hand_landmarks:
@@ -180,16 +222,43 @@ def rps(frame, gesture):
         cv2.putText(frame, str(RPS_game.getTime()), (int(frame_w / 2), int(frame_h / 2)),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     2, (255, 0, 0), 3)
-        # labelText.set(RPS.getTime())
+
         if gesture in ["rock", "paper", "scissors"]:
             RPS_game.setUserChoice(gesture)
+
+
+def functions():
+    if len(history) < 2:
+        return
+
+    current = history[-1]
+    previous = history[-2]
+
+    labels = ["thumb", "index", "middle", "ring", "pinky"]
+
+    wrist, thumb, index, middle, ring, pinky, fingers = current.values()
+    prev_wrist, prev_thumb, prev_index, prev_middle, prev_ring, prev_pinky, prev_fingers = previous.values()
+
+    if fingers == [1, 1, 0, 0, 0]:
+        pass
+    if fingers == [1, 1, 1, 1, 1]:
+        pass
+    elif fingers == [0, 1, 1, 0, 0]:
+        if (thumb[1] < prev_thumb[1]) and (index[1] < prev_index[1]):
+            control_win.scroll(-120)
+        elif (thumb[1] > prev_thumb[1]) and (index[1] > prev_index[1]):
+            control_win.scroll(120)
 
 
 def process(frame, times):
     global resultHand
 
-    leftGesture = None
-    rightGesture = None
+    left_gesture = None
+    right_gesture = None
+
+    orientation = (0, 0, 0)
+
+    pinch_distance = 0
 
     frame_h, frame_w = frame.shape[:-1]
 
@@ -222,21 +291,20 @@ def process(frame, times):
                 distances = []
 
                 for idx, landmark in enumerate(landmark_list.landmark):
-                    if idx:
-                        xs.append(landmark.x)
-                        ys.append(landmark.y)
-                        zs.append(landmark.z)
-                    else:
-                        wrist = (landmark.x, landmark.y, landmark.z)
+                    xs.append(landmark.x)
+                    ys.append(landmark.y)
+                    zs.append(landmark.z)
+                    if not idx:
+                        wrist = (xs[0], ys[0], zs[0])
 
                 minx, maxx = np.min(xs), np.max(xs)
                 miny, maxy = np.min(ys), np.max(ys)
                 minz, maxz = np.min(zs), np.max(zs)
 
-                point1 = (minx, miny, minz)
-                point2 = (maxx, maxy, maxz)
+                min_point = (minx, miny, minz)
+                max_point = (maxx, maxy, maxz)
 
-                longest_length = distance(point1, point2)
+                longest_length = distance(min_point, max_point)
 
                 for (x, y, z) in zip(xs, ys, zs):
                     dist = distance((x, y, z), wrist)
@@ -244,11 +312,34 @@ def process(frame, times):
                     distances.append(norm_dist)
 
                 if hand_idx == left_hand_idx:
-                    leftGesture = classifier.identify(distances)[0]
+                    left_gesture = classifier.identify(distances[1:])[0]
                 if hand_idx == right_hand_idx:
-                    rightGesture = classifier.identify(distances)[0]
+                    right_gesture = classifier.identify(distances[1:])[0]
 
                 fingers = get_finger_positions(distances)
+
+                key_data = {
+                    "wrist": wrist,
+                    "thumb": (xs[4], ys[4], zs[4]),
+                    "index": (xs[8], ys[8], zs[8]),
+                    "middle": (xs[12], ys[12], zs[12]),
+                    "ring": (xs[16], ys[16], zs[16]),
+                    "pinky": (xs[20], ys[20], zs[20]),
+                    "fingers": get_finger_positions(distances)
+                }
+
+                pinch_distance = distance(key_data["thumb"], key_data["index"])
+
+                x, y = key_data["thumb"][:-1]
+                relative_thumb = (int(x * frame_w), int(y * frame_h))
+                x, y = key_data["index"][:-1]
+                relative_index = (int(x * frame_w), int(y * frame_h))
+
+                cv2.line(frame, relative_thumb, relative_index, color=(255, 0, 0), thickness=2)
+
+                history.append(key_data)
+
+                functions()
 
                 for idx, finger in enumerate(fingers):
                     if finger:
@@ -256,10 +347,10 @@ def process(frame, times):
                                     cv2.FONT_HERSHEY_SIMPLEX,
                                     2, (255, 0, 0), 3)
 
-            """if leftGesture:
-                leftText.set(leftGesture)
-            if rightGesture:
-                rightText.set(rightGesture)
+            """if left_gesture:
+                leftText.set(left_gesture)
+            if right_gesture:
+                rightText.set(right_gesture)
                 
             times.append(time.time())
         else:  # if resultHand.multi_hand_landmarks
@@ -269,10 +360,10 @@ def process(frame, times):
         leftText.set("")
         rightText.set("")"""
 
-    if rightGesture is None and leftGesture:
-        gesture = leftGesture
+    if right_gesture is None and left_gesture:
+        gesture = left_gesture
     else:
-        gesture = rightGesture
+        gesture = right_gesture
 
     if mode == "rps":
         rps(frame, gesture)
@@ -282,16 +373,28 @@ def process(frame, times):
 
 def gen_frames():
     global fps, h_flip, v_flip
+    tick = 5
+    prev = time.time()
     while True:
         if cap is None:
             continue
-        success, frame = cap.read()
-        if not success:
+
+        ret = False
+
+        with lock:
+            ret, frame = cap.read()
+        if not ret:
             break
 
         # frame_h, frame_w, _ = frame.shape
 
         frame = process(frame, None)
+
+        """cur = time.time()
+                if cur - prev >= tick:
+                    if len(history):
+                        print(history[-1])
+                    prev = cur"""
 
         # Perform flips
         if h_flip:
@@ -311,9 +414,30 @@ def gen_frames():
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
+@app.route('/test', methods=['POST'])
+def testing():
+    data = request.form.get('data')
+    print(data)
+
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index_page():
+    return render_template('index.html', classes=db.getClasses())
+
+
+@app.route('/<string:page_name>')
+def goto_page(page_name):
+    # Validate and sanitize the page_name to ensure it's safe to use
+    if ".." in page_name or "/" in page_name:
+        abort(404)
+
+    template_path = f"{page_name}.html"
+
+    # Check if template exists
+    if os.path.exists(f"templates/{template_path}"):
+        return render_template(template_path)
+    else:
+        abort(404)
 
 
 @app.route('/video_feed')
@@ -338,14 +462,30 @@ def submit():
     return jsonify({"message": f"You selected {mode}"})
 
 
-@app.route('/close', methods=['POST'])
-def close():
-    print("Closing")
+def shutdown():
+    time.sleep(1)
     os.kill(os.getpid(), signal.SIGINT)
-    return 'Closing application...'
+
+
+@app.route('/close', methods=['POST'])
+def close_app():
+    print("Shutting Down")
+
+    with open("config.json", "w") as config_file:
+        json.dump(config_data, config_file)
+
+    shutdown_thread = threading.Thread(target=shutdown)
+    shutdown_thread.start()
+
+    time.sleep(5)
+
+    return jsonify(success=True, message="Closing Application")
+
+
+def run_flask_app():
+    app.run(host='0.0.0.0', port=5000)
 
 
 if __name__ == '__main__':
-    # cap = cv2.VideoCapture("http://192.168.1.55:8000")
-    change_source("http://192.168.1.55:8000")
-    app.run(host='0.0.0.0', port='5000')
+    server_thread = threading.Thread(target=run_flask_app)
+    server_thread.start()
